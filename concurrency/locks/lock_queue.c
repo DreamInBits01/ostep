645f3700f572f435a8072a638c3d35ba04b14cfd
dreaminbits01 <mohammed.knite25@gmail.com>
2025-12-05 19:26:08 +0300

    implement lock_queue && write lock_turn pseudo code

---
+++ concurrency/locks/lock_queue.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define _GNU_SOURCE
+#include <unistd.h>
+void park()
+{
+    // put caller thread to sleep
+}
+void unpark(int thread_id)
+{
+    // run the thread with the provided thread id
+}
+typedef struct queue_t
+{
+    int thread_id;
+    queue_t *next;
+} queue_t;
+
+typedef struct lock_t
+{
+    int flag;
+    int guard;
+    queue_t *queue;
+} lock_t;
+queue_t *queue_add(queue_t **queue, int data)
+{
+    queue_t *node = malloc(sizeof(queue_t));
+    if (node == NULL)
+        return NULL;
+    if (*queue == NULL)
+    {
+        *queue = node;
+        return node;
+    }
+    queue_t *current = *queue;
+    while (current->next)
+    {
+        current = current->next;
+    }
+    current->next = node;
+}
+int is_queue_empty(queue_t **queue)
+{
+    if (*queue == NULL)
+        return 1;
+    else
+        return 0;
+}
+int queue_pop(queue_t **queue)
+{
+    if ((*queue)->next == NULL)
+    {
+        int thread_id = (*queue)->thread_id;
+        free(*queue);
+        return thread_id;
+    }
+    queue_t *temp = *queue;
+    int thread_id = (*queue)->thread_id;
+    (*queue) = (*queue)->next;
+    free(temp);
+    return thread_id;
+}
+int TestAndSet(int *ptr, int new)
+{
+    int old = *ptr;
+    *ptr = new;
+    return old;
+};
+
+void lock_int(lock_t *lock)
+{
+    lock->flag = 0;
+    lock->guard = 0;
+    lock->queue = NULL;
+}
+void lock(lock_t *lock)
+{
+    while (TestAndSet(&lock->guard, 1))
+        ; // Spin if the guard is required, if not set it to 1 to aquire it to continue the operation
+
+    // Aquire the flag
+    if (lock->flag == 0)
+    {
+        lock->flag = 1;
+        lock->guard = 0;
+    }
+    else
+    {
+        /*
+            -The flag is already aquired
+            -The caller thread must be queued
+            -The guard must be let go of
+        */
+        queue_add(&lock->queue, gettid());
+        lock->guard = 0;
+        park();
+    }
+}
+void unlock(lock_t *lock)
+{
+    while (TestAndSet(&lock->guard, 1))
+        ;
+    if (is_queue_empty(&lock->queue))
+    {
+        // The queue is empty, no need to
+        lock->flag = 0;
+    }
+    else
+    {
+        unpark(queue_pop(&lock->queue));
+    }
+    lock->guard = 0;
+}
--- concurrency/locks/lock_turn.c
+++ concurrency/locks/lock_turn.c
@@ -25,8 +25,56 @@ void lock(lock_t *lock)
 }
 void unlock(lock_t *lock)
 {
-    lock->turn = FNA(&lock->turn);
+    lock->turn++;
 }
 int main()
 {
 }
+
+/*
+lock_t mutex;
+lock_init(lock); -> (ticket = 0, turn = 0)
+Lock state
+    -ticket = 0;
+    -turn = 0;
+
+-Thread 1
+lock(&mutex) <- (ticket = 0, turn = 0)
+    -FNA(&lock->ticket) <- ticket = 0
+        -FSA(&mutex->ticket) -> 0, lock state is now (ticket = 1, turn = 0)
+    -thread_turn is the returned value from FNA(&mutex->ticket) which is 0
+    -While (thread_turn != mutex->turn) <- (thread_turn = 0, mutex->turn = 0), thread WILL NOT spin
+            ; //spin
+
+-Thread 2 is running while thread 1 is holding the lock
+lock(&mutex) <- (ticket = 1, turn = 0)
+    -FNA(&lock->ticket) <- ticket = 1
+        -FSA(&mutex->ticket) -> 1, lock state is now (ticket = 2, turn = 0)
+    -thread_turn is the returned value from FNA(&mutex->ticket) which is 1
+    -While (thread_turn != mutex->turn) <- (thread_turn = 1, mutex->turn = 0), thread WILL spin
+            ; //spin
+
+-Thread 1 released the mutex
+unlock(&mutex) <- (ticket = 2, turn = 0)
+    -return (ticket = 2, turn = 1), it's now thread 2 turn
+
+-Thread 2 is running after thread 1 called unlock
+-Mutex is now (ticket = 2, turn 1)
+-Thread was stuck in spinning block
+-While (thread_turn != mutex->turn) <- (thread_turn = 1, mutex->turn = 1), thread WILL NOT spin
+        ; //spin
+
+unlock(&mutex) <- (ticket = 2, turn = 1)
+    -return (ticket = 2, turn = 2)
+
+
+
+
+
+
+
+
+
+
+
+*/
